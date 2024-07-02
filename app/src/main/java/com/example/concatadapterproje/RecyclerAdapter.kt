d74b5a21424d6f4c97ffc1122f707838be5199ba
enesa <easlangoren97@gmail.com>
2024-07-02 15:37:32 +0300

    normal-sticky

--- app/src/main/java/com/example/concatadapterproje/RecyclerAdapter.kt
+++ app/src/main/java/com/example/concatadapterproje/RecyclerAdapter.kt
@@ -11,6 +11,45 @@ import com.example.concatadapterproje.model.Database.TYPE_HEADER
 import com.example.concatadapterproje.model.Database.TYPE_ITEM
 import com.shuhart.stickyheader.StickyAdapter
 
+class RecyclerAdapter(private val totalList: ArrayList<DataItem>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    inner class ItemViewHolder(val binding: ItemRowBinding) : RecyclerView.ViewHolder(binding.root)
+    inner class HeaderViewHolder(val binding: HeaderRowBinding) : RecyclerView.ViewHolder(binding.root)
+
+    override fun getItemCount(): Int {
+        return totalList.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (totalList[position]) {
+            is DataItem.Header -> TYPE_HEADER
+            is DataItem.Item -> TYPE_ITEM
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            TYPE_ITEM -> ItemViewHolder(ItemRowBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            TYPE_HEADER -> HeaderViewHolder(HeaderRowBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+            else -> throw IllegalArgumentException("Invalid ViewType")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is ItemViewHolder -> {
+                val item = totalList[position] as DataItem.Item
+                holder.binding.tvItem.text = item.text
+            }
+            is HeaderViewHolder -> {
+                val header = totalList[position] as DataItem.Header
+                holder.binding.tvHeader.text = header.text
+            }
+        }
+    }
+}
+
+/*
 class RecyclerAdapter(private val totalList: ArrayList<DataItem>) : StickyAdapter<RecyclerView.ViewHolder, RecyclerView.ViewHolder>() {
 
     inner class ItemViewHolder(val binding: ItemRowBinding) : RecyclerView.ViewHolder(binding.root)
@@ -70,3 +109,4 @@ class RecyclerAdapter(private val totalList: ArrayList<DataItem>) : StickyAdapte
         return HeaderViewHolder(HeaderRowBinding.inflate(LayoutInflater.from(parent.context), parent, false))
     }
 }
+ */
